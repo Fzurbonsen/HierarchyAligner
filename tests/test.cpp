6bac3347ebd63e9d08816d5ec06ceab5fadc3675
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-06-26 03:02:47 +0200

    edlib_gwfa combination run

--- tests/test.cpp
+++ tests/test.cpp
@@ -1686,8 +1686,8 @@ int main() {
     // run_standard_tests("./test_cases/reference_graph.gfa", "./test_cases/node_list_3.txt", "", match, mismatch, gap_open, gap_extend, stderr);
     // run_standard_tests("./test_cases/reference_graph.gfa", "./test_cases/node_list_4.txt", "", match, mismatch, gap_open, gap_extend, stderr);
 
-    FILE* outputFile = fopen("./files/benchmarks_ecoli_1/gwfa.csv", "w");
-    run_benchmark("./test_cases/ecoli_graph.gfa", "./test_cases/ecoli_node_list.txt", "./test_cases/1000.new.sim.txt", match, mismatch, gap_open, gap_extend, outputFile, projectA_get_timed_alignment_gwfa);
+    FILE* outputFile = fopen("./files/benchmarks_ecoli_1/gwfa_edlib.csv", "w");
+    run_benchmark("./test_cases/ecoli_graph.gfa", "./test_cases/ecoli_node_list.txt", "./test_cases/1000.new.sim.txt", match, mismatch, gap_open, gap_extend, outputFile, projectA_get_timed_alignment_edlib_gwfa);
     fclose(outputFile);
     // outputFile = fopen("./files/benchmarks_ecoli_1/gssw.csv", "w");
     // run_benchmark("./test_cases/ecoli_graph.gfa", "./test_cases/ecoli_node_list.txt", "./test_cases/1000.new.sim.txt", match, mismatch, gap_open, gap_extend, outputFile, projectA_get_timed_alignment_gssw);
